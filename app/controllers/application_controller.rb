d4e2204f9412f4bee0ea9a4a20b27417858b815c
Graham Johnson <grahamgjohnson@gmail.com>
2019-03-26 12:37:34 -0400

    Created tables, user model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,27 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+    
+    helper_method :current_user, :logged_in?
+
+    def current_user
+        User.find_by(session_token: session[:session_token])
+    end
+
+    def signed_in?
+        !!current_user
+    end
+
+    def sign_in(user)
+        session[:session_token] = user.reset_session_token!
+    end
+
+    def sign_out
+        current_user.reset_session_token!
+        session[:session_token] = nil
+    end
+
+    def ensure_logged_in
+        redirect_to new_session_url unless signed_in?
+    end
+
 end
