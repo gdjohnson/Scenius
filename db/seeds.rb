54e78ffcb05eb4b64d00c6d0af8b72ff6243d457
Graham Johnson <grahamgjohnson@gmail.com>
2019-03-26 20:34:45 -0400

    User seed test

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+author = User.new([{ name: 'graham', password: '1234567'])
