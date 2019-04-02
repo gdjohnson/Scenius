ff65aeb2db54344038c47dfb7f2bbd0d5019a1bd
Graham Johnson <suspendedreason@gmail.com>
2019-04-01 21:23:11 -0400

    Fixed seeding, tweaked header CSS

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+# Drop the database and start over if you need fresh ID nums
+Album.delete_all
+User.delete_all
+Artist.delete_all
+
 #Users
 graham = User.create(username: "graham", password: "123456")
 guest = User.create(username: "temerity", password: "123456")
@@ -13,7 +19,6 @@ guest = User.create(username: "temerity", password: "123456")
 #Artists
 roxymusic = Artist.create(name: "Roxy Music")
 robertaflack = Artist.create(name: "Roberta Flack")
-
 
 #Albums
 fleshnblood = Album.create(title: "Flesh + Blood", artist_id: 1, year: 1980, artwork_url: "https://upload.wikimedia.org/wikipedia/en/thumb/7/71/Flesh_and_Blood_album_cover.jpg/220px-Flesh_and_Blood_album_cover.jpg")
