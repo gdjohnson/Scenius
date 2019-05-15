04fe5c7610f1185775071b5fb0fa64d997f65fa6
Graham Johnson <suspendedreason@gmail.com>
2019-05-15 17:11:57 -0400

    Recompiling

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -57,4 +57,9 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+
+
+
+  config.assets.prefix =  "/dev-assets"
 end
